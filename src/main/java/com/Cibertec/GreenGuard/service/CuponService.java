e7c1176d7e84bfdfaf403b2bc1813d95cdfadf93
Vector404 <victornarazas2005@gmail.com>
2025-12-08 14:17:31 -0500

    Actualizacion de la DB

--- src/main/java/com/Cibertec/GreenGuard/service/CuponService.java
+++ src/main/java/com/Cibertec/GreenGuard/service/CuponService.java
@@ -1,5 +1,7 @@
 package com.Cibertec.GreenGuard.service;
 
+import com.Cibertec.GreenGuard.model.Categoria;
+import com.Cibertec.GreenGuard.model.Cupon;
 import com.Cibertec.GreenGuard.repository.ICuponRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,32 @@ public class CuponService {
 
     @Autowired
     ICuponRepository cuponRepo;
+
+    private String generarCodigoCupon(){
+        Long
+    }
+
+    public Cupon crearNuevoCupon(Cupon cupon){
+
+        Cupon cupoRegistrado = new Cupon();
+
+        cupoRegistrado.setNombreCupon(cupon.getNombreCupon());
+
+        String regexSoloLetras = "^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$";
+
+        if (!cupon.getDescCupon().contains(regexSoloLetras)){
+            throw new RuntimeException("Solo se permite la entrada de letras no numeros!");
+        }
+
+        cupoRegistrado.setDescCupon(cupon.getDescCupon());
+
+        Categoria categoria = new Categoria();
+        categoria.setIdCate(cupon.getCategoria().getIdCate());
+
+        cupoRegistrado.setCategoria(categoria);
+
+    }
+
+
+
 }
